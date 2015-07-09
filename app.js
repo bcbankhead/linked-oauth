4afb6070588aee06210c910c4c26a4a49a45863f
barrybankhead <bcbankhead@gmail.com>
2015-07-09 13:18:41 -0600

    installed cookies and env

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var session = require('cookie-session');
+require('dotenv').load()
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set(session({
+  keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]
+}))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
